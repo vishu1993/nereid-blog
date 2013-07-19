5ae29c5b871f9c9df318eb50910c04293aa3b918
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2013-07-19 18:02:46 +0530

    Setup travis for nereid blog. #2017
    Updated .travis.yml file with notification email and flake8
    
    Review ID: 271001

--- setup.py
+++ setup.py
@@ -81,7 +81,9 @@ major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = []
+requires = [
+    'nereid >= 2.4, <2.5',
+]
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append(
