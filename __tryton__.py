b0f192cd5724fbf5813f9a8367550b1b00cc9114
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-07-22 11:39:00 +0530

    Release version 2.4.2.0

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 # This is CONFIDENTIAL code to be retained within Openlabs and GradUit
 {
     'name': 'Nereid Blog',
-    'version': '2.4.2.0dev',
+    'version': '2.4.2.0',
     'author': 'Openlabs Technologies & Consulting (P) Ltd.',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
