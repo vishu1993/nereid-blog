4878a6d1177c5bf585816e26a73a3c2b60bc314d
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-07-22 11:36:23 +0530

    Bump version to 2.4.2.0dev

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 # This is CONFIDENTIAL code to be retained within Openlabs and GradUit
 {
     'name': 'Nereid Blog',
-    'version': '2.4.1.0',
+    'version': '2.4.2.0dev',
     'author': 'Openlabs Technologies & Consulting (P) Ltd.',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
